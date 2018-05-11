06b34a0c5aca7faef8851dbc7b689be945176795
rickmi <rickmi@users.noreply.github.com>
2018-05-11 15:38:24 +1000

    Update index.php

--- src/index.php
+++ src/index.php
@@ -21,7 +21,7 @@
                 <p>Your PHP application is now running on a container in AWS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
-                        $myfile = fopen("/var/www/my-vol/date", "r") or die("");
+                        $myfail = fopen("/var/www/my-vol/date", "r") or die("");
                         echo fread($myfile,filesize("/var/www/my-vol/date"));
                         fclose($myfile);
                 ?>
